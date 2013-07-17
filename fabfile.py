b0f04e9e15fb1c9b4cff0fbf79a1afff6b4a4b15
Brian Boucheron <brian@boucheron.org>
2013-07-17 13:32:10 -0400

    tweaks, recover lost post

--- fabfile.py
+++ fabfile.py
@@ -15,4 +15,6 @@ def serve():
 def publish():
     clean()
     local('hyde gen -c site-production.yaml')
+    local('yui-compressor ./deploy/brian/media/js/plugins.js -o ./deploy/brian/media/js/plugins.js')
+    local('for file in $(find ./deploy -type f -name "*.html" -o -name "*.css" -o -name "*.js"); do zopfli $file; touch -c --reference=$file $file.gz; done')
     local('rsync -r ./deploy/ bert@bean:/home/bert/public/boucheron.org/')
