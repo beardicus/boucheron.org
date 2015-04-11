d47ee32f49296babc29a78338e882ba419f562fd
Brian Boucheron <brian@boucheron.org>
2015-04-10 21:38:28 -0400

    switch to pelican

--- README.md
+++
@@ -1,6 +0,0 @@
-These are the innards of [http://boucheron.org/brian][1]. I'm currently using [hyde][] to generate said site and push it up to a static server.
-
-
-[1]: http://boucheron.org/brian
-[hyde]: https://github.com/hyde/hyde
-
--- fabfile.py
+++ fabfile.py
@@ -1,69 +1,42 @@
 from fabric.api import *
+import os
+import shutil
+
+env.deploy_path = 'output'
+env.output_branch = 'output'
 
 
 def clean():
-    local('rm -rf ./deploy')
+    """Remove generated files"""
+    if os.path.isdir(env.deploy_path):
+        shutil.rmtree(env.deploy_path)
+        os.makedirs(env.deploy_path)
 
-def gen():
+
+def build():
+    """Build local version of site"""
+    local('pelican -s localconf.py')
+
+
+def rebuild():
+    """`clean` then `build`"""
     clean()
-    local('hyde gen')
+    build()
+
 
 def serve():
-    gen()
-    local('hyde serve')
+    """Serve site at http://localhost:8000/"""
+    os.chdir(env.deploy_path)
+    local('python -m pelican.server')
+
+
+def preview():
+    """Build production version of site"""
+    local('pelican -s publishconf.py')
+
 
 def publish():
-    clean()
-    local('hyde gen -c site-production.yaml')
-    local('yui-compressor ./deploy/brian/media/js/plugins.js -o ./deploy/brian/media/js/plugins.js')
-    local('for file in $(find ./deploy -type f -name "*.html" -o -name "*.css" -o -name "*.js" -o -name "*.xml"); do zopfli --i100 $file; touch -c --reference=$file $file.gz; done')
-    local('rsync -r --delete --progress ./deploy/ bert@bean:/home/bert/public/boucheron.org/')
-
-def draft():
-    from datetime import datetime
-    from glob import glob
-    from iptcinfo import IPTCInfo
-    import os
-
-    stamp = datetime.now()
-    imagepath = '/home/bert/Projects/boucheron.org/images/'
-    staticpath = '/home/bert/Projects/boucheron.org/content/brian/media/static/'
-    blogpath = '/home/bert/Projects/boucheron.org/content/brian/{}/{}/'.format(stamp.year, stamp.month)
-
-    files = glob('/home/bert/Dropbox/Transfer/Draft/*.jpg')
-
-    blogdir = os.path.dirname(blogpath)
-    if not os.path.exists(blogdir):
-        os.makedirs(blogdir)
-
-    targetfile = open(blogpath + "draft.html", 'w')
-
-    targetfile.write("""---
-title: 
-created: !!timestamp '{}'
-tags: []
----
-
-
-""".format(str(stamp).split('.')[0]))
-
-    for index, jpg in enumerate(files):
-        title = IPTCInfo(jpg).data['object name']
-        newname = stamp.strftime('%Y%m%d-') + str(index) + '.jpg'
-       
-        # move jpgs to originals storage
-        os.rename(jpg, imagepath + newname)
-        
-        # resize and crush originals to media dir
-        local('convert -resize "800>" -strip -interlace Plane -quality 90% -filter Lanczos -unsharp 0.5x0.5+2+0.005 {} {}'.format(imagepath + newname, staticpath + newname))
-        local('jpgcrush ' + staticpath + newname)
-
-        # add image markdown to the draft
-        markdown = '![{}](/brian/media/static/{})\n\n'
-        targetfile.write(markdown.format(title, newname))
-
-
-    # close file, open in gvim
-    targetfile.close()
-    local('gvim ' + blogpath + 'draft.html')
-
+    """Build and push to output branch"""
+    local('pelican -s publishconf.py')
+    local("ghp-import -b {github_pages_branch} {deploy_path}".format(**env))
+    local("git push origin {github_pages_branch}".format(**env))
