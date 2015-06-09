5e41bbc263ee9482e2edc14976171e1681475981
Brian Boucheron <brian@boucheron.org>
2015-06-08 21:19:54 -0400

    rabble rabble

--- content/articles/cheddar-tasting.md
+++ content/articles/cheddar-tasting.md
@@ -12,4 +12,3 @@ I've been sitting on these photos for about a month now, but somehow word still 
 I cut the wheel up into eights and vacuum packed them all. Hopefully I'll be able to wait out the full year before tasting the rest, and we'll see if it changes much in that time. In the meantime, the weather is getting nice and cool, and I'm finding myself in the kitchen more often on the weekends, cooking and baking and brewing. Hopefully the cheesemaking will resume in October, and I'll be able to put up enough cheese over winter to satisfy the snacking appetite of family and friends for the next year.
 
 ![First Cheddar Tasting]({filename}/images/7820045254.jpg)
-
--- localconf.py
+++ localconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Brian Boucheron'
 SITENAME = u'Brian Boucheron'
-SITEURL = ''
+SITEURL = 'http://localhost:8000'
 
 PATH = 'content'
 THEME = 'theme'
@@ -12,24 +12,26 @@ THEME = 'theme'
 TIMEZONE = 'America/New_York'
 DEFAULT_LANG = u'en'
 
-DEFAULT_PAGINATION = 5
+DEFAULT_PAGINATION = False
 DEFAULT_ORPHANS = 1
 TYPOGRIFY = True
 
 ARTICLE_PATHS = ['articles']
 
 SLUGIFY_SOURCE = 'title'
-ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}'
-ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}.html'
-PAGE_URL = '{slug}'
-PAGE_SAVE_AS = '{slug}.html'
+
 INDEX_SAVE_AS = 'index.html'
 ARCHIVES_SAVE_AS = 'archive.html'
 
-PAGINATION_PATTERNS = (
-    (1, '{base_name}/', '{base_name}/index.html'),
-    (2, '{base_name}/{number}', '{base_name}/{number}.html'),
-)
+ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}.html'
+
+PAGE_URL = '{slug}'
+PAGE_SAVE_AS = '{slug}.html'
+
+TAG_URL = 'tag/{slug}'
+TAG_SAVE_AS = 'tag/{slug}.html'
+TAGS_SAVE_AS = 'tags.html'
 
 YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
 MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
@@ -37,14 +39,12 @@ MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
 # Turn off some pages we don't need
 AUTHOR_SAVE_AS = ''
 AUTHORS_SAVE_AS = ''
-TAG_SAVE_AS = ''
-TAGS_SAVE_AS = ''
 CATEGORY_SAVE_AS = ''
 CATEGORIES_SAVE_AS = ''
-FEED_ALL_ATOM = ''
-CATEGORY_FEED_ATOM = ''
 AUTHOR_FEED_ATOM = ''
 AUTHOR_FEED_RSS = ''
+
+DIRECT_TEMPLATES = ('index', 'tags', 'archives')
 
 # Disable feed generation during development
 FEED_ALL_ATOM = None
@@ -52,4 +52,4 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Use document-relative URLs during development
-RELATIVE_URLS = True
+#RELATIVE_URLS = True
