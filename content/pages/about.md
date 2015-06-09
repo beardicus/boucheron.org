f1e63038518e259b06634fefce1adaaad1d4c95d
Brian Boucheron <brian@boucheron.org>
2015-06-09 17:07:51 -0400

    zug zug

--- content/pages/about.md
+++ content/pages/about.md
@@ -1,6 +1,7 @@
 Title: Hello? What's this?
 Summary: About page for Brian Boucheron
 Slug: about
+Tags: about
 
 This shall be written from the future. In the meantime, you may find a brief and outdated profile at [my about.me page][1]
 
--- localconf.py
+++ localconf.py
@@ -7,21 +7,35 @@ SITENAME = u'Brian Boucheron'
 SITEURL = 'http://localhost:8000'
 
 PATH = 'content'
+ARTICLE_PATHS = ['articles']
 THEME = 'theme'
 
 TIMEZONE = 'America/New_York'
 DEFAULT_LANG = u'en'
-
-DEFAULT_PAGINATION = False
-DEFAULT_ORPHANS = 1
 TYPOGRIFY = True
-
-ARTICLE_PATHS = ['articles']
-
 SLUGIFY_SOURCE = 'title'
 
 INDEX_SAVE_AS = 'index.html'
+TAGS_SAVE_AS = 'tags.html'
 ARCHIVES_SAVE_AS = 'archive.html'
+YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
+AUTHORS_SAVE_AS = ''
+AUTHOR_SAVE_AS = ''
+CATEGORIES_SAVE_AS = ''
+CATEGORY_SAVE_AS = ''
+
+
+DIRECT_TEMPLATES = ('index', 'tags', 'archives')
+
+DEFAULT_PAGINATION = 5
+DEFAULT_ORPHANS = 1
+
+PAGINATED_DIRECT_TEMPLATES = ('index',)
+PAGINATION_PATTERNS = (
+    (1, '{base_name}/', '{base_name}/index.html'),
+    (2, '{base_name}/{number}', '{base_name}/{number}.html'),
+)
 
 ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}'
 ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}.html'
@@ -31,25 +45,15 @@ PAGE_SAVE_AS = '{slug}.html'
 
 TAG_URL = 'tag/{slug}'
 TAG_SAVE_AS = 'tag/{slug}.html'
-TAGS_SAVE_AS = 'tags.html'
-
-YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
-MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
-
-# Turn off some pages we don't need
-AUTHOR_SAVE_AS = ''
-AUTHORS_SAVE_AS = ''
-CATEGORY_SAVE_AS = ''
-CATEGORIES_SAVE_AS = ''
-AUTHOR_FEED_ATOM = ''
-AUTHOR_FEED_RSS = ''
-
-DIRECT_TEMPLATES = ('index', 'tags', 'archives')
 
 # Disable feed generation during development
+FEED_ATOM = None
+FEED_RSS = None
 FEED_ALL_ATOM = None
+FEED_ALL_RSS = None
 CATEGORY_FEED_ATOM = None
+CATEGORY_FEED_RSS = None
+TAG_FEED_ATOM = None
+TAG_FEED_RSS = None
 TRANSLATION_FEED_ATOM = None
-
-# Use document-relative URLs during development
-#RELATIVE_URLS = True
+TRANSLATION_FEED_RSS = None
